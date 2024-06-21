13840ac481c8ef7d1285ba79c5ffc85dff8b6c34
RichaArora1989 <richaar@skillup.tech>
2024-06-22 01:11:30 +0530

    adding cart and createslice

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
+import cartReducer from './CreatSlice';
 
  const store = configureStore({
     reducer: {
