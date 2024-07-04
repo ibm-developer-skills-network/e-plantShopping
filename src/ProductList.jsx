71086140792fc18182f297061d75b5ef2d976878
ril-rajiv <rajiv41.kumar@ril.com>
2024-07-04 05:52:32 -0400

    added productcategories list with respective products lists

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -231,7 +231,7 @@ function ProductList() {
    }
     return (
         <div>
-             <div className="navbar" style={styleObj}>
+            <div className="navbar" style={styleObj}>
             <div className="tag">
                <div className="luxury">
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
@@ -248,14 +248,27 @@ function ProductList() {
                 <div> <a href="#" style={styleA}>Plants</a></div>
                 <div> <a href="#" style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
             </div>
+            </div>
+
+            <div className="product-grid">
+                {plantsArray.map((category, index) => (
+                    <div key={index}>
+                        <h1><div>{category.category}</div></h1>
+                        <div className="product-list">
+                            {category.plants.map((plant, plantIndex) => (
+                                <div className="product-card" key={plantIndex}>
+                                    <img className="product-image" src={plant.image} alt={plant.name} />
+                                    <div className="product-title">{plant.name}</div>
+                                    /*Similarly like the above plant.name show other details like description and cost*/
+                                    <button  onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+
         </div>
-
-        <div className="product-grid">
-
-
-        </div>
-
-    </div>
     );
 }
 
