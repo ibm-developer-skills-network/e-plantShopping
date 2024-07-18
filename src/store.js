e57926d14180e28f972af58a2b099e6f3b61189f
RicArora1991 <richaaeri9988@gmail.com>
2024-07-18 13:04:32 +0530

    cart slice

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CreateSlice';
+import cartReducer from './CartSlice';
 
  const store = configureStore({
     reducer: {
