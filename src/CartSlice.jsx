1a43156ef267baf062ab6fc7457d79b58548f30c
vkul1234 <vedant.kulkarni242003@gmail.com>
2024-07-20 22:58:54 +0530

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,30 +4,30 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [],
-    totalItemCount:0, // Initialize items as an empty array
+    totalItemCount: 0, // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-  const { name, image, cost } = action.payload;
-  const existingItem = state.items.find(item => item.name === name);
-  if (existingItem) {
-    existingItem.quantity++;
-  } else {
-    state.items.push({ name, image, cost, quantity: 1 });
-  }
-  state.totalItemCount = state.items.reduce((total, item) => total + item.quantity, 0);
-},
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+      state.totalItemCount = state.items.reduce((total, item) => total + item.quantity, 0);
+    },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
-   state.totalItemCount = state.items.reduce((total, item) => total + item.quantity, 0);
+      state.items = state.items.filter(item => item.name !== action.payload);
+      state.totalItemCount = state.items.reduce((total, item) => total + item.quantity, 0);
     },
     updateQuantity: (state, action) => {
-        const { name, quantity } = action.payload;
-const itemToUpdate = state.items.find(item => item.name === name);
-if (itemToUpdate) {
-  itemToUpdate.quantity = quantity;
-}
-    state.totalItemCount = state.items.reduce((total, item) => total + item.quantity, 0);
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
+      state.totalItemCount = state.items.reduce((total, item) => total + item.quantity, 0);
     },
   },
 });
