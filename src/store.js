7e1ff1d1d11b3ec8d1c9e365d4ad3a5e116b6bfe
JbellMD <91991220+JbellMD@users.noreply.github.com>
2024-07-21 13:39:45 -0400

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './src/CartSlice';
+import cartReducer from './CartSlice';
 
  const store = configureStore({
     reducer: {
