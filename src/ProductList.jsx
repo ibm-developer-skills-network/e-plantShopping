ce7cd824f8a8b060ca2ba12bf01f8c9dc08eee78
JbellMD <91991220+JbellMD@users.noreply.github.com>
2024-07-21 13:39:08 -0400

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addItem } from './src/CartSlice';
-import './src/ProductList.css';
+import { addItem } from './CartSlice';
+import './ProductList.css';
 
 function ProductList() {
   const [addedToCart, setAddedToCart] = useState({});
