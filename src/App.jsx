7b367a92b8b6287cec6ed448f3baa5c4cf060101
JbellMD <91991220+JbellMD@users.noreply.github.com>
2024-07-21 12:06:25 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // App.jsx
 import React, { useState } from 'react';
-import ProductList from './src/ProductList';
-import CartItem from './src/CartItem';
-import './src/App.css';
-import AboutUs from './src/AboutUs';
+import ProductList from '/src/ProductList';
+import CartItem from '/src/CartItem';
+import '/src/App.css';
+import AboutUs from '/src/AboutUs';
 
 function App() {
   const [showProductList, setShowProductList] = useState(false);
