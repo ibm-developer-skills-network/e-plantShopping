a688898760fbe9d2099da6d358b16557a8eba42b
JbellMD <91991220+JbellMD@users.noreply.github.com>
2024-07-21 11:40:27 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-
+// App.jsx
 import React, { useState } from 'react';
 import ProductList from './ProductList';
+import CartItem from './CartItem';
 import './App.css';
 import AboutUs from './AboutUs';
 
 function App() {
-  
   const [showProductList, setShowProductList] = useState(false);
+  const [showCart, setShowCart] = useState(false);
 
   const handleGetStartedClick = () => {
     setShowProductList(true);
+    setShowCart(false);
+  };
+
+  const handleContinueShopping = () => {
+    setShowCart(false);
   };
 
   return (
@@ -17,29 +23,31 @@ function App() {
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
         <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
+          <div className="landing_content">
+            <h1>Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
+            <button className="get-started-button" onClick={handleGetStartedClick}>
+              Get Started
+            </button>
+          </div>
           <div className="aboutus_container">
-          <AboutUs/>
+            <AboutUs />
           </div>
-          </div>
-
+        </div>
       </div>
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList />
+        {showCart ? (
+          <CartItem onContinueShopping={handleContinueShopping} />
+        ) : (
+          <ProductList />
+        )}
+        <button className="view-cart-button" onClick={() => setShowCart(!showCart)}>
+          {showCart ? 'Back to Products' : 'View Cart'}
+        </button>
       </div>
     </div>
   );
 }
 
 export default App;
-
-
-
