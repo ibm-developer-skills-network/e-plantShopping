bbc4f4cfc31acf68bf81e00c5d825a2505a4ef3d
JbellMD <91991220+JbellMD@users.noreply.github.com>
2024-07-21 11:47:45 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './store.js';
+import store from '/e-plantShopping/src/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
