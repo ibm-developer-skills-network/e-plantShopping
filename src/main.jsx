2f886a765a45ca6eea056edf9f6c93e41f070d66
JbellMD <91991220+JbellMD@users.noreply.github.com>
2024-07-21 13:39:34 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 // main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App';
-import './src/index.css';
+import App from './App';
+import './index.css';
 import { Provider } from 'react-redux';
-import store from './src/store';
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
