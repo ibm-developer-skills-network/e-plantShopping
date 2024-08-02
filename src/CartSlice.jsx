2f81ef155b9611b5835fcfc1d6ddffb6cc4926c6
cagriefe <cagriefe34@gmail.com>
2024-08-02 16:56:24 +0300

    updated cartslice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,23 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const newItem = action.payload;
+      const existingItem = state.items.find(item => item.name === newItem.name);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ ...newItem, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
