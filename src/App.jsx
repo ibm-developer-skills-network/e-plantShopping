51051ce3388302003726e7231aa49a5151ec2c12
Whalley7 <hallettwendy@gmail.com>
2024-08-11 16:04:12 -0400

    Reducer added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 import AboutUs from './AboutUs';
 
 function App() {
-  
+
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,44 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+        let totalAmount = 0;
+    cart.forEach(item => {
+      totalAmount += parseFloat(item.cost.replace('$', ''))  * item.quantity;
+    });
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+     onContinueShopping();
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
 
 
   const handleIncrement = (item) => {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity +1}));
   };
 
   const handleDecrement = (item) => {
-   
+     if(item.quantity > 1) {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity -1}));
+    }else {
+      dispatch(removeItem (item));
+    }
   };
 
   const handleRemove = (item) => {
+      dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+  return parseFloat(item.cost.replace('$', '')) * item.quantity;
   };
+  
 
   return (
     <div className="cart-container">
