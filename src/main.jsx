fd168ee572716a589e312e42c081b00e13efbfa7
MobyScript <90425990+MobyScript@users.noreply.github.com>
2024-08-14 17:01:05 +0300

    Added: Routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <Router>
+        <Route path="/shoppingreact" component={App} />
+      </Router>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
