d7cd3a26063e1ebc5f3f32e8decc22c8442c3756
MobyScript <90425990+MobyScript@users.noreply.github.com>
2024-08-14 17:08:44 +0300

    Reverted Changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,10 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store.js";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <Route path="/shoppingreact" component={App} />
-      </Router>
+      <App />
     </Provider>
   </React.StrictMode>
 );
