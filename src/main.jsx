0c67f59bba551c6c2eb36a2ecfedcf0711908ca9
Teyasinhasan <teyasinhasan@gmail.com>
2024-08-15 12:45:04 -0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+import { HashRouter as Router } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+    <Router>
     <App />
+    </Router>
     </Provider>
   </React.StrictMode>,
 )
