e01f3cf805545bd2d94b93e8b01dcf246519d21d
Teyasin S Hasan <108761340+Teyasinhasan@users.noreply.github.com>
2024-08-15 11:52:44 -0500

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,36 +1,84 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { addItem, removeItem, updateQuantity, clearCart } from './CartSlice';
 import './CartItem.css';
+ 
 
 const CartItem = ({ onContinueShopping }) => {
+
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
-  // Calculate total amount for all products in the cart
+
   const calculateTotalAmount = () => {
- 
+    const parseCost = (cost) => {
+      return parseFloat(cost.replace(/[^0-9.-]+/g, '')) || 0;
+    };
+  
+    // Calculate total amount based on the cart items
+    return cart.reduce((total, item) => {
+      const itemCost = parseCost(item.cost);
+
+      return total + (itemCost * item.quantity);
+
+    }, 0);
+
+  };
+  
+  const calculateTotalCost = (item) => {
+    const parseCost = (cost) => {
+      return parseFloat(cost.replace(/[^0-9.-]+/g, '')) || 0;
+    };
+  
+      const itemCost = parseCost(item.cost);
+      return (itemCost * item.quantity);
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping();
   };
 
-
-
   const handleIncrement = (item) => {
+    
+    if (item) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+      }
+      
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+      } else {
+        dispatch(removeItem(item))
+      }
+
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
+    
   };
 
-  // Calculate total cost based on quantity for an item
-  const calculateTotalCost = (item) => {
-  };
+
+
+
+  const handleCheckoutShopping = (e) => {
+    e.preventDefault();
+
+    /* In Redux, you cannot directly manipulate the state; 
+    you have to use actions and reducers  
+
+    I tried directly manipulating the cart (state.items) and it error
+    */
+
+  alert('Thank you for the purchase !');
+
+  dispatch(clearCart());
+
+};
+
+
 
   return (
     <div className="cart-container">
@@ -43,9 +91,14 @@ const CartItem = ({ onContinueShopping }) => {
               <div className="cart-item-name">{item.name}</div>
               <div className="cart-item-cost">{item.cost}</div>
               <div className="cart-item-quantity">
-                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                <button className="cart-item-button
+
+                cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+
                 <span className="cart-item-quantity-value">{item.quantity}</span>
+
                 <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+
               </div>
               <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
               <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
@@ -55,14 +108,15 @@ const CartItem = ({ onContinueShopping }) => {
       </div>
       <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+        <button className="get-started-button" onClick={(e) => onContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
+
 
 
