84eb03d5ef2951368f2eaa3ca3d74dedff8fece2
Teyasinhasan <teyasinhasan@gmail.com>
2024-08-15 17:34:19 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShoppingNew/",
+  base: "/e-plantShoppingNew",
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom', '@reduxjs/toolkit'],
+},
+});
