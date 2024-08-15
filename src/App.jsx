d7fec6091c80a3c6f2132b0439db95c81d17baae
Teyasinhasan <teyasinhasan@gmail.com>
2024-08-15 12:20:33 -0500

    Deploy to Github Pages

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,15 @@ function App() {
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
         <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
+        <div className="landing_content">
+        <h1>Welcome To Paradise Nursery</h1>
           <div className="divider"></div>
           <p>Where Green Meets Serenity</p>
-         
+        
           <button className="get-started-button" onClick={handleGetStartedClick}>
             Get Started
           </button>
-         </div>
+        </div>
           <div className="aboutus_container">
           <AboutUs/>
           </div>
