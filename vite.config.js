5023f6634d0797eb682fc74500cfd36e805e74c9
Teyasinhasan <teyasinhasan@gmail.com>
2024-08-15 17:48:53 -0500

    fixes

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShoppingNew",
   plugins: [react()],
+  base: "/e-plantShoppingNew",
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom', '@reduxjs/toolkit'],
 },
