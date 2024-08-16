6c44b4a4ffe2ab00fb09eed5bfd17603f292b818
kgomotso <kgomotsomkhawane2001@gmail.com>
2024-08-16 03:00:49 +0200

    vscode edit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
