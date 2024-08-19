80b0791f82d0b3200ce7d1d36c742d8a02aa666e
Whalley7 <hallettwendy@gmail.com>
2024-08-19 22:00:17 +0000

    Change AboutUs CSS

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import "./ProductList.css";
-import CartItem from "./CartItem";
+import CartItem from './CartItem';
 import { addItem } from "./CartSlice";
 
 function ProductList() {
