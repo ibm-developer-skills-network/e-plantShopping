d2f7f1ab71336f490f968525abfa02666bd8366c
revellt <zvutozsg@gmail.com>
2024-08-21 01:42:20 -0400

    end of project 1

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
