54ed701d6fd074ce60d3dbadbaa1b87cca9e0206
enrik.lea <enrico.lenta@gmail.com>
2024-08-23 07:44:19 -0400

    ProductList

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -268,8 +268,23 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
-
+            {plantsArray.map((category, index) => (
+                <div key={index}>
+                    <h1><div>{category.category}</div></h1>
+                    <div className="product-list">
+                        {category.plants.map((plant, plantIndex) => (
+                        <div className="product-card" key={plantIndex}>
+                            <img className="product-image" src={plant.image} alt={plant.name} />
+                            <div className="product-title">{plant.name}</div>
+                            {/*Similarly like the above plant.name show other details like description and cost*/}
+                            <div className="product-description">{plant.description}</div>
+                            <div className="product-price">{plant.cost}</div>
+                            <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                        </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
         </div>
  ) :  (
     <CartItem onContinueShopping={handleContinueShopping}/>
