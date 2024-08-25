862e7bfe8c674326fe4ee42d5e3d5642b84cc80c
enrik.lea <enrico.lenta@gmail.com>
2024-08-25 10:39:42 -0400

    cartQuantityCount

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,6 +4,8 @@ import CartItem from './CartItem';
 import { addItem, removeItem, updateQuantity} from './CartSlice';
 import { useDispatch } from "react-redux";
 
+let cartQuantityCount = 0;
+
 function ProductList() {
     const dispatch = useDispatch();
     const [showCart, setShowCart] = useState(false); 
@@ -258,6 +260,7 @@ const handleAddToCart = (product) => {
        ...prevState,
        [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
      }));
+     cartQuantityCount = cartQuantityCount + 1;
 };
 
     return (
@@ -277,7 +280,11 @@ const handleAddToCart = (product) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div><a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                        <text id="cartQuantityCount" font-size="80px" y="154" x="115" fill="white">{cartQuantityCount}</text>
+                    </svg></h1></a>
+                </div>
             </div>
         </div>
         {!showCart? (
