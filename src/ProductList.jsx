2bf4f05629c063dee17c1e509d291e464df32c39
enrik.lea <enrico.lenta@gmail.com>
2024-08-26 10:54:32 -0400

    About

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -271,8 +271,8 @@ const handleAddToCart = (product) => {
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                <a href="/" style={{textDecoration:'none'}}>
                         <div>
-                    <h3 style={{color:'white'}}>Paradise Nursery</h3>
-                    <i style={{color:'white'}}>Where Green Meets Serenity</i>
+                    <h3 style={{color:'white'}}>Plants4U</h3>
+                    <i style={{color:'white'}}>Your new plants e-commerce platform!</i>
                     </div>
                     </a>
                 </div>
