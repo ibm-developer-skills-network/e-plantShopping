3089756881cc6fc80ba06fc3ea1655f1a23d3e6d
FekherJ <91023618+FekherJ@users.noreply.github.com>
2024-09-05 21:02:11 +0200

    adding to cart fixed

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -263,17 +263,43 @@ const handlePlantsClick = (e) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
             </div>
         </div>
         {!showCart? (
         <div className="product-grid">
 
+            {plantsArray.map((CartItem, index) => (
+                console.log(CartItem)
+            ))}
 
+            <table>
+                <tbody>
+                    {plantsArray.map((category, index) => (
+                        <div key={index}>
+                            <h1><div>{category.category}</div></h1>
+                                <div class="product-list">
+                                    {category.plants.map((plant, plantIndex) => (
+                                        <div className='product-card' key = {plantIndex}>
+                                            <img className = 'product-image' src = {plant.image} alt={plant.name}/>
+                                            <div className = 'product-title'>{plant.name}</div>
+                                            <div className = 'product-title'>{plant.description}</div>
+                                            <div className=  'product-title'>${plant.cost}</div>
+                                            <button className='product-button' onClick={() =>  handleAddToCart(plant)}> Add to cart </button>
+                                        </div>
+                                    ))}
+                                </div>
+                        </div>
+                    ))}
+                                         
+                </tbody>
+            </table>
+           
         </div>
  ) :  (
     <CartItem onContinueShopping={handleContinueShopping}/>
-)}
+)} 
     </div>
     );
 }
