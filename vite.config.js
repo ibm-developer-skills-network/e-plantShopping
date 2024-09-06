5efe7ab9abf87a4dddba6ed9cd686fe0f4a5eea2
FekherJ <91023618+FekherJ@users.noreply.github.com>
2024-09-06 17:55:08 +0200

    update cart

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/e-plantShopping/',  // Make sure this matches your repo name exactly
   plugins: [react()],
-});
+})
