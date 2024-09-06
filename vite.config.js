4552308e4dbc124c0a463513151627fa8a7a75ff
FekherJ <91023618+FekherJ@users.noreply.github.com>
2024-09-06 13:15:43 +0200

    adding to cart fix 0.9

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/<your-repo-name>/',  // Replace <your-repo-name> with your actual repo name
+  base: '/e-plantShopping/',  // Make sure this matches your repo name exactly
   plugins: [react()],
 });
-
