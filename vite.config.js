282e4abf2d2f45ff71922a8e7a8ec97dcd73446c
Arslanr369 <Arslanriaz785@gmail.com>
2024-09-08 09:21:40 +0500

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/React-e-plantShopping",
   plugins: [react()],
-})
+});
