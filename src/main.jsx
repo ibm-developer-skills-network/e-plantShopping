48acf07b6ee221b30da070ec64e71a90f3bfac20
Letlhogonolo Chauke <PITROSCHAUKE24@GMAIL.COM>
2024-09-10 13:54:52 +0200

    Adding browser router to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
-  </React.StrictMode>,
+  <BrowserRouter basename="/e-PlantShopping">
+    <React.StrictMode>
+      <Provider store={store}>
+      <App />
+      </Provider>
+    </React.StrictMode>,
+  </BrowserRouter>
 )
