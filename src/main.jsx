37607c9de866d7c8e01d7193798bfbaeee1da4d5
Letlhogonolo Chauke <PITROSCHAUKE24@GMAIL.COM>
2024-09-10 14:29:51 +0200

    Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <BrowserRouter basename="/e-plantShopping">
