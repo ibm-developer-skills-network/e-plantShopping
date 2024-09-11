e1588c36ab87c661c096450f5230101f7f90acb6
goro5685 <gorogore060@gmail.com>
2024-09-11 22:43:20 +0300

    vite set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-    </Provider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
