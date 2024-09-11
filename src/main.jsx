0d2670f0c6442dd3cfd7c5b7e875088061647d22
Abdulrahman Abulghaith <81337359+goro5685@users.noreply.github.com>
2024-09-11 23:05:05 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { Provider } from 'react-redux'
+import store from './store.js'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </StrictMode>,
+    </Provider>
+  </React.StrictMode>,
 )
