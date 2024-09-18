f659dae3838740646dfe80f5b6243fc60bb8f6cd
AdityaSK08 <aditya.22110902@viit.ac.in>
2024-09-19 04:51:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/e-plantShopping",
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
+  },
 })
