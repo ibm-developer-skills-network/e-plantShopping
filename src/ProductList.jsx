bb9e30a11a3dae2f7bc9b9eed4a3662894f49771
allencpu <allenproof@yahoo.com>
2024-09-29 03:27:55 -0700

    Update ProductList.jsx
    adding functions to buttons

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,19 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
+import { addItem } from './CartSlice';
 import CartItem from './CartItem';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const {addedToCart, setAddedToCart} = useState([]);
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name]: true,
+        }));
+    };
 
     const plantsArray = [
         {
@@ -268,6 +278,24 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
+
+{plantsArray.map((category, index) => (
+    <div key={index}>
+        <h1><div>{category.category}</div></h1>
+        <div className="product-list">
+            {category.plants.map((plant, plantIndex) => (
+            <div className="product-card" key={plantIndex}>
+                <img className="product-image" src={plant.image} alt={plant.name} />
+                <div className="product-title">{plant.name}</div>
+                <div className="product-price">{plant.cost}</div>
+                {/*Similarly like the above plant.name show other details like description and cost*/}
+                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+            </div>
+            ))}
+        </div>
+    </div>
+    
+    ))}
 
 
         </div>
