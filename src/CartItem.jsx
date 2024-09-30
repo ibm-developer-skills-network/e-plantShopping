47112876e769bf1259c9add74c91de32e45abae6
Zakariabh <bhaibizakaria@gmail.com>
2024-09-30 11:56:09 -0400

    initial commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,39 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    for (const item of cart) {
+      const price = parseFloat(item.cost.replace('$', ''));
+      total += price * item.quantity;
+    }
+    return total.toFixed(2);
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+  onContinueShopping();
   };
 
 
 
-  const handleIncrement = (item) => {
+  const handleIncrement = (item) => {dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity > 1) {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+  } else {
+    dispatch(removeItem(item)); 
+  }
   };
 
-  const handleRemove = (item) => {
+  const handleRemove = (item) => { dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const price = parseFloat(item.cost.replace('$', '')); 
+  return (price * item.quantity).toFixed(2);
   };
 
   return (
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,21 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+import { useDispatch, useSelector } from 'react-redux';
 function ProductList() {
+    const dispatch = useDispatch(); 
+  const totalQuantity = useSelector(state => state.cart.totalQuantity);
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+           ...prevState,
+           [product.name]: true, 
+         }));
+      };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -245,8 +256,10 @@ const handlePlantsClick = (e) => {
    const handleContinueShopping = (e) => {
     e.preventDefault();
     setShowCart(false);
+    
   };
     return (
+        
         <div>
              <div className="navbar" style={styleObj}>
             <div className="tag">
@@ -268,7 +281,20 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
+                {plantsArray.map((category, index) => (
+    <div key={index}>
+        <h1><div>{category.category}</div></h1>
+        <div className="product-list">
+            {category.plants.map((plant, plantIndex) => (
+            <div className="product-card" key={plantIndex}>
+                <img className="product-image" src={plant.image} alt={plant.name} />
+                <div className="product-title">{plant.name}</div>
+                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+            </div>
+            ))}
+        </div>
+    </div>
+    ))}
 
         </div>
  ) :  (
