f480841f931c32222df336043c9dfdda380a2aa2
Ehbraheem <ehbraheem@gmail.com>
2024-10-01 15:28:20 -0400

    Cart reducer

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css';
 import CartItem from './CartItem';
-import { addItem } from './CartSlice';
+import { addItem, updateQuantity, removeItem } from './CartSlice';
 import { useSelector, useDispatch } from 'react-redux';
 
 function ProductList() {
