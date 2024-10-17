bd454f5d09c1e14b83cac5d973ead46eb1aff736
ameliah23 <ameliahermawant@gmail.com>
2024-10-17 19:58:42 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React-e-plantShopping",
+  base: "/e-plantShopping",
   plugins: [react()],
 })
