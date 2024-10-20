75d49b3b07faed85a6d6a82ced1b9e3fe7bca95c
Wendy Hallett <hallettwendy@gmail.com>
2024-10-20 20:03:51 +0100

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import "./ProductList.css";
-import CartItem from "./CartItem";
-import { addItem } from "./CartSlice";
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import './ProductList.css';
+import CartItem from './CartItem';
+import { addItem } from './CartSlice';
 
 function ProductList() {
   const dispatch = useDispatch();
