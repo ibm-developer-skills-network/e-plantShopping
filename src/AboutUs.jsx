7535f881f9e89f30dddc3da1f97b57c9c9447961
Wendy Hallett <hallettwendy@gmail.com>
2024-10-20 17:25:03 +0100

    Update AboutUs.jsx

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -20,7 +20,6 @@ function AboutUs() {
         home or office.
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
-
       <p className="about-us-content">
         Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
         beauty of nature right at your doorstep.
