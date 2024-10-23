15f0a44f48e06d8f3138756a28149e7341f5641d
Frumosi <kkone.devops@gmail.com>
2024-10-23 11:46:53 -0400

    Updated

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -18,13 +18,19 @@ export const CartSlice = createSlice({
     
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
+        const { name, quantity } = action.payload;
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+          itemToUpdate.quantity = quantity;
+        }
 
     
     },
   },
-});
+);
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
