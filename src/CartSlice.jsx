1d86f26959ade7af0e756fb16567e7589dc221ec
Anam020990 <anamikaa@skillup.tech>
2024-10-23 18:35:56 +0530

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,1 +1,23 @@
-// Enter your code here
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+    addItem: (state, action) => {
+    
+    },
+    removeItem: (state, action) => {
+    },
+    updateQuantity: (state, action) => {
+
+    
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
