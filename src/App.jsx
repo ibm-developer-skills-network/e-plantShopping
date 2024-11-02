67cf192d4087fc80b78a3600231267d9e95178f7
Cayla4224 <crodri0297@gmail.com>
2024-11-02 18:09:22 -0400

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ function App() {
   const handleGetStartedClick = () => {
     setShowProductList(true);
   };
+  const handleReturnClick = () => {
+    setShowProductList(false);
+  }
 
   return (
     <div className="app-container">
@@ -33,7 +36,7 @@ function App() {
 
       </div>
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList />
+        <ProductList toLanding={handleReturnClick} />
       </div>
     </div>
   );
