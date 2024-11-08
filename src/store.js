1565eddbc0e1777fa3561159d5989fd97d848e27
Rumi <98488477+rumi127@users.noreply.github.com>
2024-11-08 00:35:07 +0000

    Export configured Redux store and integrate Redux with application components

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice'; // Import the cart reducer
+import cartReducer from './CartSlice';
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer, // Add the cart reducer to the store
-  },
+// Configure the store with the cart reducer
+const store = configureStore({
+    reducer: {
+        cart: cartReducer, // cart slice of state managed by cartReducer
+    },
 });
+
+// Export the configured store as the default export
+export default store;
