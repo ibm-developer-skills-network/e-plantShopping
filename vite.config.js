7176e1c112dcbb8a2c4947539a53e862669875ea
Mesut <mozdinc@gmail.com>
2024-11-11 23:05:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/shoppingreact",
-  base: "/e-plantShopping/",
+  base: "/e-plantShopping/shoppingreact",
   plugins: [react()],
 })
