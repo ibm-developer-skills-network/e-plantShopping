6c76f914b7d427aee8396437c4294747156f5705
Mesut <mozdinc@gmail.com>
2024-11-13 15:06:48 +0100

    Number of product in the cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -266,7 +266,7 @@ const handlePlantsClick = (e) => {
   };
     return (
         <div>
-             <div className="navbar" style={styleObj}>
+            <div className="navbar" style={styleObj}>
             <div className="tag">
                <div className="luxury">
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
@@ -276,24 +276,28 @@ const handlePlantsClick = (e) => {
                     <i style={{color:'white'}}>Where Green Meets Serenity</i>
                     </div>
                     </a>
+                    
                 </div>
               
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a>  </div>
                 <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                <div><h3 style={{ color: 'white' }}>{cartItemsCount}</h3></div>
                         <h1 
-                            className='cart'>  
+                            className='cart'>       
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" 
-                                id="IconChangeColor" height="68" width="68"><rect width="156" height="156" 
-                                fill="none"> </rect><circle cx="80" cy="216" r="12"></circle>
-                                
+                                id="IconChangeColor" height="68" width="68">
+                                <rect width="156" height="156" fill="none"> </rect>
+                                <circle cx="80" cy="216" r="12"></circle>
                                 <circle cx="184" cy="216" r="12"></circle>
                                 <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" 
                                   fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute">
                                 </path>
                                 </svg>
                         </h1>
+                        
+                        
                     </a>
                 </div>
                 
