30c1850fc3674281b280185a1e2fccd2b655f33b
Axel Serrano <axelinsh2003@gmail.com>
2024-11-14 15:49:43 -0600

    ending the final project

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,24 +7,24 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      const {name, image, cost} = action.payload;
-      const existingItem = state.items.find(item => item.name === name );
-      if (existingItem){
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
         existingItem.quantity++;
       } else {
-        state.items.push({name, image, cost, quantity: 1})
+        state.items.push({ name, image, cost, quantity: 1 });
       }
     },
     removeItem: (state, action) => {
-      state.item = state.items.filter(item => item.name !== action.payload)
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-      const {name, quantity} = action.payload;
+      const { name, quantity } = action.payload;
       const itemToUpdate = state.items.find(item => item.name === name);
-      if (itemToUpdate){
+      if (itemToUpdate) {
         itemToUpdate.quantity = quantity;
       }
-    
+
     },
   },
 });
