77ec26233729ea4f01a6b2bd4da64689a03b58be
GillianoA <agard.gilliano@gmail.com>
2024-11-14 17:17:05 -0500

    redux final

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { addItem } from './CartSlice';
 
 export const CartSlice = createSlice({
   name: 'cart',
@@ -7,13 +8,25 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload;
+
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            state.items.push({ name, image, cost, quantity: 1 });
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
+        const { name, quantity } = action.payload;
 
-    
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+        }
     },
   },
 });
