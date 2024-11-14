d607b633fdfc03be1567b3432a953eb812003985
Mesut <mozdinc@gmail.com>
2024-11-14 22:18:24 +0100

    change vite and package.json

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/shoppingreact",
-  base: "/mesutozdinc/shoppingcart",
+  base: "/shoppingcart",
   plugins: [react()],
 })
