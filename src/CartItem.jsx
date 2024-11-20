21574e5a7a8e9cf1c5371eae00f42a29727ffeff
JDoh31 <jdoh.business@gmail.com>
2024-11-20 17:23:12 -0500

    all up to event handling

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+    cart.forEach((item) => {
+        totalAmount += item.cost * item.quantity
+    });
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ ...item, quantity: item.quantity + 1 }));
+    setTotalQuantity(prevTotal => prevTotal + 1);
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity === 1) {
+        dispatch(removeItem(item.name));
+        setTotalQuantity(prevTotal => prevTotal - 1);
+    } else {
+        dispatch(updateQuantity({ ...item, quantity: item.quantity - 1 }));
+        setTotalQuantity(prevTotal => prevTotal - 1);
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * item.cost;
   };
+
 
   return (
     <div className="cart-container">
