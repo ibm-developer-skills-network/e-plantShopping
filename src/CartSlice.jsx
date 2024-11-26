7903f0a1daef873a2d2bb46a1c00c75c8f0100c2
David Shumway <david_shumway@live.com>
2024-11-25 20:02:52 -0800

    Task 2 - complete

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,17 +7,27 @@ export const CartSlice = createSlice({
     },
     reducers: {
         addItem: (state, action) => {
-
+            const item = action.payload;
+            const existingItem = state.items.find(i => i.id === item.id);
+            if (existingItem) {
+                existingItem.quantity += item.quantity;
+            } else {
+                state.items.push(item);
+            }
         },
         removeItem: (state, action) => {
+            const itemId = action.payload;
+            state.items = state.items.filter(item => item.id !== itemId);
         },
         updateQuantity: (state, action) => {
-
-
+            const { id, quantity } = action.payload;
+            const existingItem = state.items.find(item => item.id === id);
+            if (existingItem) {
+                existingItem.quantity = quantity;
+            }
         },
     },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
-
 export default CartSlice.reducer;
