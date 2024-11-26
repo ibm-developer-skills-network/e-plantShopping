4bea261a97c057650b85ecc8e34a02acd5e23451
David Shumway <david_shumway@live.com>
2024-11-26 07:46:01 -0800

    Navbar CSS corrected and simplified

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = ({ onShowPlants, onShowCart, totalQuantity }) => (
     <div className="navbar">
         <div className="tag">
             <div className="luxury">
-                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="Paradise Nursery Logo" />
-                <a href="/" style={{ textDecoration: 'none' }}>
+                <img src="" alt="Paradise Nursery Logo" />
+                <a href="/">
                     <div>
                         <h2 className="luxury-text">Paradise Nursery</h2>
                         <i className="luxury-text">Where Green Meets Serenity</i>
@@ -16,17 +16,13 @@ const Navbar = ({ onShowPlants, onShowCart, totalQuantity }) => (
         </div>
         <div className="ul">
             <div>
-                <a href="#" onClick={onShowPlants} className="a">Plants</a>
+                <a href="#" onClick={onShowPlants} className="plants-link">Plants</a>
             </div>
             <div>
-                <a href="#" onClick={onShowCart} className="a">
+                <a href="#" onClick={onShowCart} className="cart-link">
                     <h1 className='cart'>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
-                            <rect width="156" height="156" fill="none"></rect>
-                            <circle cx="80" cy="216" r="12"></circle>
-                            <circle cx="184" cy="216" r="12"></circle>
-                            <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path>
-                            <text x="50%" y="50%" textAnchor="middle" fill="#faf9f9" fontSize="86" dy=".3em">{totalQuantity}</text>
+
                         </svg>
                     </h1>
                 </a>
