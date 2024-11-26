4daa97b4ee039ab48925fbc4201b83e14488c588
KAESZAR <cesdev.s@outlook.com>
2024-11-26 17:21:03 -0500

    cambios, en Product, Items and Car

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,68 +1,96 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
-import './CartItem.css';
+import React from 'react';  
+import { useSelector, useDispatch } from 'react-redux';  
+import { removeItem, updateQuantity } from './CartSlice';  
+import './CartItem.css';  
 
-const CartItem = ({ onContinueShopping }) => {
-  const cart = useSelector(state => state.cart.items);
-  const dispatch = useDispatch();
+const CartItem = ({ onContinueShopping }) => {  
+  const cart = useSelector(state => state.cart.items);  
+  const dispatch = useDispatch();  
 
-  // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
-  };
+  // Calculate total amount for all products in the cart  
+  const calculateTotalAmount = () => {  
+    return cart.reduce((total, item) => total + calculateTotalCost(item), 0);  
+  };  
 
-  const handleContinueShopping = (e) => {
-   
-  };
+  const handleContinueShopping = () => {  
+    // Call the function passed from parent to continue shopping  
+    onContinueShopping();  
+  };  
 
+  const handleIncrement = (item) => {  
+    dispatch(updateQuantity(item.id, item.quantity + 1));  
+  };  
 
+  const handleDecrement = (item) => {  
+    if (item.quantity > 1) {  
+      dispatch(updateQuantity(item.id, item.quantity - 1));  
+    } else {  
+      // If quantity is 1, remove item from the cart  
+      dispatch(removeItem(item.id));  
+    }  
+  };  
 
-  const handleIncrement = (item) => {
-  };
+  const handleRemove = (item) => {  
+    dispatch(removeItem(item.id));  
+  };  
 
-  const handleDecrement = (item) => {
-   
-  };
+  // Calculate total cost based on quantity for an item  
+  const calculateTotalCost = (item) => {  
+    return item.cost * item.quantity;  
+  };  
 
-  const handleRemove = (item) => {
-  };
+  const handleCheckoutShopping = () => {  
+    alert('Functionality to be added for future reference');  
+  };  
 
-  // Calculate total cost based on quantity for an item
-  const calculateTotalCost = (item) => {
-  };
-
-  return (
-    <div className="cart-container">
-      <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
-      <div>
-        {cart.map(item => (
-          <div className="cart-item" key={item.name}>
-            <img className="cart-item-image" src={item.image} alt={item.name} />
-            <div className="cart-item-details">
-              <div className="cart-item-name">{item.name}</div>
-              <div className="cart-item-cost">{item.cost}</div>
-              <div className="cart-item-quantity">
-                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
-                <span className="cart-item-quantity-value">{item.quantity}</span>
-                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
-              </div>
-              <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
-              <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
-      <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
-        <br />
-        <button className="get-started-button1">Checkout</button>
-      </div>
-    </div>
-  );
-};
+  return (  
+    <div className="cart-container">  
+      <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount().toFixed(2)}</h2>  
+      <div>  
+        {cart.map(item => (  
+          <div className="cart-item" key={item.id}>  
+            <img className="cart-item-image" src={item.image} alt={item.name} />  
+            <div className="cart-item-details">  
+              <div className="cart-item-name">{item.name}</div>  
+              <div className="cart-item-cost">Unit Price: ${item.cost.toFixed(2)}</div>  
+              <div className="cart-item-quantity">  
+                <button   
+                  className="cart-item-button cart-item-button-dec"  
+                  onClick={() => handleDecrement(item)}  
+                >  
+                  -  
+                </button>  
+                <span className="cart-item-quantity-value">{item.quantity}</span>  
+                <button   
+                  className="cart-item-button cart-item-button-inc"  
+                  onClick={() => handleIncrement(item)}  
+                >  
+                  +  
+                </button>  
+              </div>  
+              <div className="cart-item-total">Subtotal: ${calculateTotalCost(item).toFixed(2)}</div>  
+              <button   
+                className="cart-item-delete"   
+                onClick={() => handleRemove(item)}  
+              >  
+                Delete  
+              </button>  
+            </div>  
+          </div>  
+        ))}  
+      </div>  
+      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>  
+      <div className="continue_shopping_btn">  
+        <button className="get-started-button" onClick={handleContinueShopping}>  
+          Continue Shopping  
+        </button>  
+        <br />  
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>  
+          Checkout  
+        </button>  
+      </div>  
+    </div>  
+  );  
+};  
 
 export default CartItem;
-
-
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/https://github.com/KAESZAR/e-plantShopping.git",
+
   plugins: [react()],
 })
