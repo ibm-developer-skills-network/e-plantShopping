39a02ecd899f41eb3ac8c894478a421935a3f5e6
afonseca00 <alexandref.rodrigues8@gmail.com>
2024-11-29 13:02:58 +0000

    Project finished

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -282,7 +282,7 @@ const handleAddToCart = (plant) => {
             <div className="tag">
                <div className="luxury">
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-               <a href="/" style={{textDecoration:'none'}}>
+               <a href="/e-plantShopping/" style={{textDecoration:'none'}}>
                         <div>
                     <h3 style={{color:'white'}}>Paradise Nursery</h3>
                     <i style={{color:'white'}}>Where Green Meets Serenity</i>
