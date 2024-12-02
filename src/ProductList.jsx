5f8ff486572307fcb7c667af17e779690d4e83cb
rachit4018 <pandyarachit6@gmail.com>
2024-12-01 22:16:53 -0500

    added plant card

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -268,7 +268,22 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
+            {plantsArray.map((category, index) => (
+                <div key = {index}>
+                    <h1><div>{category.category}</div></h1>
+                    <div className="product-list">
+                        {category.plants.map((plant,plantIndex)=>(
+                            <div className='product-card' key={plantIndex}>
+                                <img className='product-image' src={plant.image} alt={plant.name}></img>
+                                <div className='product-title'>{plant.name}</div>
+                                <div className='product-description'>{plant.description}</div>
+                                <div className='product-cost'>{plant.cost}</div>
+                                <button className='product-button' onClick={() => handleAddToCart(plant)}></button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
 
         </div>
  ) :  (
