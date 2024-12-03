0a14b9bb8a0d8a2acdb94cfab83268331b582585
rachit4018 <pandyarachit6@gmail.com>
2024-12-02 23:19:27 -0500

    updated cartreducer with add,removce and update function

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,7 +4,7 @@ import CartItem from './CartItem';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -246,6 +246,13 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+  const handleAddToCart = (product) => {
+  dispatch(addItem(product));
+  setAddedToCart((prevState) => ({
+     ...prevState,
+     [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+   }));
+};
     return (
         <div>
              <div className="navbar" style={styleObj}>
