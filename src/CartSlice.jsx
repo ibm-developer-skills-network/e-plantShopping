bb401b9f64e745fa774eb17c4527f19fb47331ff
8KINTARO8 <nicola.puccetti84@gmail.com>
2024-12-07 13:04:14 -0500

    implemented reducers in cartslice jsx addtocart removeitem updatequantity

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -5,14 +5,33 @@ export const CartSlice = createSlice({
   initialState: {
     items: [], // Initialize items as an empty array
   },
+  //Puccetti Nicola ...for this isn't used state directly
+  //but is a first test 
   reducers: {
     addItem: (state, action) => {
-    
+        const existingItem = state.items.find((item)=> item.name === action.payload.name);
+        if(existingItem){
+            existingItem.quantity += 1;
+        }
+        else{
+            existingItem.items = state.items.push({...action.payload, quantity: 1});
+        }
     },
     removeItem: (state, action) => {
+        const existingItem = state.items.find((item)=> item.name === action.payload.name);
+        if(existingItem){
+            existingItem.items = state.items.filter((item)=> item.name !== action.payload.name);
+        }
+        else{
+            return console.log("error when removing items from cart")
+        }
     },
     updateQuantity: (state, action) => {
-
+        const {name,quantity} = action.payload;
+        const itemToUpdate = state.items.find((item)=>item.name === name);
+        if(itemToUpdate){
+            itemToUpdate.quantity = quantity;
+        }
     
     },
   },
