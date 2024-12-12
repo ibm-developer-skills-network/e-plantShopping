b037b114635ad91ceb5d264bffdada513eb8a1a3
yj99yj <yongyongjoo@gmail.com>
2024-12-12 02:31:13 -0500

    작yj

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
+import productsReducer from './productsSlice'; 
  const store = configureStore({
     reducer: {
         cart: cartReducer,
+        products: productsReducer,
     },
 });
 export default store
