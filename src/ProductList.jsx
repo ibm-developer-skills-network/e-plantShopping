3476426642ebd40b6cf9b96bc1abef9a170e6da9
8KINTARO8 <nicola.puccetti84@gmail.com>
2024-12-13 12:50:22 -0500

    into productlist jsx file added totalitemcart function this is implemented into svg tag for return total items quantity added to cart this value is shown into nav bar in center of cart icon

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,6 +7,7 @@ import {addItem} from './CartSlice'
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const cart= useSelector(state=>state.cart.items);
     const dispatch = useDispatch();
     const plantsArray = [
         {
@@ -262,6 +263,10 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+  const totalItemsCart = ()=>{
+    const totalQuantityOutput = cart.reduce((total,item)=> total+ item.quantity,0);
+    return totalQuantityOutput;
+  }
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -279,7 +284,7 @@ const handlePlantsClick = (e) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path><text x="50%" y="50%" textAnchor="middle" fill="white" fontSize="60" dy=".3em"> {totalItemsCart()} </text></svg></h1></a></div>
             </div>
         </div>
         {!showCart? (
