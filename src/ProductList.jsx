4a1df22e09629f6578e1101b9cc8d8409a34ce1b
profitprem7 <profitprem7@gmail.com>
2024-12-22 01:31:25 -0500

    Commit for react project

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -9,6 +9,8 @@ function ProductList() {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
 
+
+    
     const plantsArray = [
         {
             category: "Air Purifying Plants",
