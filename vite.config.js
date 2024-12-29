798e34a3f85050e6f3891abe2f3cac8d2ed3765c
ZyS1b <zybertsib@gmail.com>
2024-12-30 04:04:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantshopping",
+  base: "/e-plantShopping",
   plugins: [react()],
 })
