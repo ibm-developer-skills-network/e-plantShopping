940c71018cc2ddcbb9117f217cc8c19f97038510
ZyS1b <zybertsib@gmail.com>
2024-12-30 02:05:11 +0800

    Finished Task 4: Integrate Redux functionality in your components

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,14 @@
-import React, { useState,useEffect } from 'react';
-import './ProductList.css'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './ProductList.css';
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
 
 function ProductList() {
+    const dispatch = useDispatch();
+    const cartItems = useSelector((state) => state.cart.items);
     const [showCart, setShowCart] = useState(false); 
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [showPlants, setShowPlants] = useState(false); 
     const [addedToCart, setAddedToCart] = useState({});
 
     const plantsArray = [
