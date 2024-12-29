e2763bf483c5051046517955c2827bad356c248a
ZyS1b <zybertsib@gmail.com>
2024-12-30 04:56:57 +0800

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -67,7 +67,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
