49678bce90898991e82f0d0e87cdc7cb4b4bc0b7
Oustamanolakis <oustamanolakis@gmail.com>
2024-12-31 14:59:30 +0100

    Update vite.config.js
    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+base: "/e-plantShopping",
 export default defineConfig({
   base: "/shoppingreact",
   plugins: [react()],
