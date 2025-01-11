fe2281c073791061a45fd682bba41ccdef82565c
Natasha <natasha-x-lopez@github.com>
2025-01-11 18:29:55 -0500

    Updated App.jsx and store.js to reflect CartItem and ProductList changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,22 @@
-
-import React, { useState } from 'react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store'; // Adjust the import path as needed
 import ProductList from './ProductList';
-import './App.css';
-import AboutUs from './AboutUs';
+import CartItem from './CartItem';
 
 function App() {
-  
-  const [showProductList, setShowProductList] = useState(false);
-
-  const handleGetStartedClick = () => {
-    setShowProductList(true);
-  };
-
   return (
-    <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
-
-      </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+    <Provider store={store}>
+      <div className="App">
         <ProductList />
+        <CartItem />
       </div>
-    </div>
+    </Provider>
   );
 }
 
 export default App;
+
 
 
 
--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+import cartReducer from './CartSlice'; // Adjust the import path as needed
+
+export default configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
 });
-export default store
+
