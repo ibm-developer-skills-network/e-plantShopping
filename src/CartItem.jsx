8fc4c519e26240979a8085c6d91accb5206c4e94
Natasha <natasha-x-lopez@github.com>
2025-01-11 18:48:39 -0500

    Refined increment and decrement functions in CartItem component to use updateQuantity and handle removal at zero quantity

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity, increment, decrement } from './CartSlice';
+import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
@@ -73,6 +73,7 @@ const CartItem = ({ onContinueShopping }) => {
 };
 
 export default CartItem;
+
 
 
 
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -50,3 +50,4 @@ export default CartSlice.reducer;
 
 
 
+
