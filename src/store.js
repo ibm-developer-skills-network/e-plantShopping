fbfd34c77ee62ef79aa4b0c684ac331b9c219bc9
Natasha <natasha-x-lopez@github.com>
2025-01-11 13:28:58 -0500

    Update

--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 // src/redux/store.js
 
-import { createStore } from 'redux';
-import rootReducer from './reducers'; // Path to your root reducer
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './CartSlice'; // Path to your cart reducer
 
-const store = createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = configureStore({
+    reducer: {
+        cart: cartReducer,
+    },
+});
 
 export default store;
+
 
 
