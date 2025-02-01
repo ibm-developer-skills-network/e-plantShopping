4147a0dfa5f0b4b514bf1cc35f1519b1b1bbf3d4
MANIL BELKESSAM <122458218+ItheManil@users.noreply.github.com>
2025-02-01 21:59:01 +0100

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import ProductList from './ProductList';
+import './App.css';
+import AboutUs from './AboutUs';
+
+function App() {
+  const [showProductList, setShowProductList] = useState(false);
+
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
+  };
+
+  return (
+    <div className="app-container">
+      {/* Landing page section */}
+      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+        <div className="background-image"></div>
+        <div className="content">
+          <div className="landing_content">
+            <h1>Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
+            <button className="get-started-button" onClick={handleGetStartedClick}>
+              Get Started
+            </button>
+          </div>
+
+          {/* About Us section */}
+          <div className="aboutus_container">
+            <AboutUs />
+          </div>
+        </div>
+      </div>
+
+      {/* Product list section */}
+      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+        {showProductList && <ProductList />} {/* Conditionally render ProductList */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
