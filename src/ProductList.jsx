3ff8662913524e7fa67a5012db17cea45c868e3f
The-morning-star23 <kumarshubh263@gmail.com>
2025-02-15 12:10:30 -0500

    Made the changes to ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -246,6 +246,12 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+
+  const handleAddToCart = (plant) => {
+        dispatch(addItem(plant)); // Dispatching the action to add item to the cart
+        setAddedToCart(prevState => ({ ...prevState, [plant.name]: true }));
+    };
+
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -268,7 +274,22 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
+            {plantsArray.map((category, index) => (
+            <div key={index}>
+                <h1><div>{category.category}</div></h1>
+                <div className="product-list">
+                    {category.plants.map((plant, plantIndex) => (
+                    <div className="product-card" key={plantIndex}>
+                        <img className="product-image" src={plant.image} alt={plant.name} />
+                        <div className="product-title">{plant.name}</div>
+                        <div className="product-description">{plant.description}</div>
+                        <div className="product-cost">{plant.cost}</div>
+                        <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+            </div>
+            ))}
+        </div>
+    </div>
+    ))}
 
         </div>
  ) :  (
