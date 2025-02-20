876a1b23c19255cd53ced91a1b18f2822e5594e3
Isaac2509 <issye3060@gmail.com>
2025-02-20 09:33:49 +0300

    Productlist.jsx

--- Productlist.jsx
+++ Productlist.jsx
@@ -1,19 +1,47 @@
-import { useDispatch } from 'react-redux';
-import { addItem } from './path/to/CartSlice';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/CartSlice"; // Import addItem action
 
-const ProductList = ({ products }) => {
+const plantsArray = [
+  {
+    id: 1,
+    name: "Aloe Vera",
+    image: "/images/aloe-vera.jpg",
+    description: "A succulent plant with healing properties.",
+    cost: "$10",
+  },
+  {
+    id: 2,
+    name: "Snake Plant",
+    image: "/images/snake-plant.jpg",
+    description: "A hardy plant that improves air quality.",
+    cost: "$15",
+  },
+];
+
+const ProductList = () => {
   const dispatch = useDispatch();
+  const [addedToCart, setAddedToCart] = useState({});
 
   const handleAddToCart = (plant) => {
-    dispatch(addItem(plant));
+    dispatch(addItem(plant)); // Dispatch plant data to Redux store
+    setAddedToCart((prevState) => ({ ...prevState, [plant.name]: true })); // Update state
   };
 
   return (
-    <div>
-      {products.map(plant => (
-        <div key={plant.id}>
+    <div className="product-grid">
+      {plantsArray.map((plant) => (
+        <div key={plant.id} className="product-card">
+          <img src={plant.image} alt={plant.name} />
           <h3>{plant.name}</h3>
-          <button onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+          <p>{plant.description}</p>
+          <p>{plant.cost}</p>
+          <button
+            onClick={() => handleAddToCart(plant)}
+            disabled={addedToCart[plant.name]}
+          >
+            {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
+          </button>
         </div>
       ))}
     </div>
