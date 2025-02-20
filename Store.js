dc0a084166e2c1747a8f62c2130b841c05ee77da
Isaac2509 <issye3060@gmail.com>
2025-02-20 10:30:08 +0300

    Update Store.js

--- Store.js
+++ Store.js
@@ -8,3 +8,15 @@ const store = configureStore({
 });
 
 export default store;
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './CartSlice';  // Import the reducer for cart management
+
+// Configure the Redux store
+const store = configureStore({
+    reducer: {
+        cart: cartReducer,  // Assign the cartReducer to manage cart state
+    },
+});
+
+// Export the store to use it throughout the app
+export default store;
