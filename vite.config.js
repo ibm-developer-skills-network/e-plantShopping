65a3fb7fe5fa32a491c01c52dfce37dc1183ab44
Call Simba <32197243+callsimba@users.noreply.github.com>
2025-02-21 15:38:39 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-base: '/e-plantShopping/',
-  plugins: [react()],
+  base: '/plantShopping/', // Make sure this matches your repo name exactly
+  plugins: [react()]
 })
