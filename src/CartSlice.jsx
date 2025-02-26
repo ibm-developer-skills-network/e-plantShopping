3daaeb97c00c6b1fba94f721d83ee31673cb12c4
Blessy <blessypaul28@gmail.com>
2025-02-26 23:41:49 +0530

    changes for shopping cart project

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,16 +4,32 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    numOfItems: 0
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+      state.numOfItems += 1;
     },
     removeItem: (state, action) => {
+      const { name, quantity } = action.payload;
+      state.items = state.items.filter(item => item.name !== name);
+      state.numOfItems -= quantity;
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        const differenceQuantity = quantity - itemToUpdate.quantity;
+        state.numOfItems += differenceQuantity;
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
