9d98c3ef7a929c9e5803ad0765569468560ac8cb
meedoune <smedoune01@gmail.com>
2025-03-03 15:40:54 -0500

    latest version

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/shoppingreact",
+base: "/e-plantShopping",
   plugins: [react()],
 })
